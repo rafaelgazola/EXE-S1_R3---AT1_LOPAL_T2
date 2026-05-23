76a5bd34a130dc26bfca774b0dab812c43a07bd0
rafaelgazola <rafaelrubimgazola@gmail.com>
2026-05-23 15:21:20 -0300

    feat:add_Exe-py

--- Exe_11.py
+++ Exe_11.py
@@ -5,4 +5,4 @@ numero = int(input("Senhor me forneça um número para a tabuada de 1 a 10: "))
 for i in range(1,11): # logica para repetir a linha pela quantidade de numero pedido
   multi = numero * i #multiplicação = multiplicando pelo número pedido
 
-  print(f"{numero} x {i} = {multi}") #saida de dados, mostrando o resultado pedido
+  print(f"{numero} x {i} = {multi}") #saida de dados, mostrando o resultado pedido.
